cfc3f7657394f19c710f551538a148047c675a0c
Divya Badugu <divyaaa1812@gmail.com>
2023-04-15 17:05:05 -0700

    changes

--- scripts/index.js
+++ scripts/index.js
@@ -28,9 +28,20 @@ const initialCards = [
 const editButton = document.querySelector(".js-profile-edit-button");
 const editProfileModal = document.querySelector(".js-modal");
 const modalCloseButton = document.querySelector(".js-modal-close-button");
+//Extract title and subtitle elements
+const profileTitle = document.querySelector(".profile__title");
+const profileSubtitle = document.querySelector(".profile__subtitle");
+//Extract input fields from modal
+const profileTitleInputField = document.querySelector("#profile-title-input");
+const profileSubtitleInputField = document.querySelector(
+  "#profile-subtitle-input"
+);
 
 function openModal() {
   editProfileModal.classList.add("modal_opened");
+  //set values for input fileds after form is opened
+  profileTitleInputField.value = profileTitle.textContent;
+  profileSubtitleInputField.value = profileSubtitle.textContent;
 }
 editButton.addEventListener("click", openModal);
 
@@ -38,3 +49,12 @@ function closeModal() {
   editProfileModal.classList.remove("modal_opened");
 }
 modalCloseButton.addEventListener("click", closeModal);
+
+for (i = 0; i <= initialCards.length; i++) {}
+
+function getCardElement(data) {
+  const cardElement = document.querySelector("#card-template").cloneNode(true);
+  console.log(cardElement);
+  const cardTitle = document.querySelector(".card__title");
+  const images = document.querySelector(".card__image");
+}
