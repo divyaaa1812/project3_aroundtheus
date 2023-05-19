e021017529da01de9c2a8c25296fc1960139b476
Divya Badugu <divyaaa1812@gmail.com>
2023-05-19 09:18:09 -0700

    changes

--- scripts/index.js
+++ scripts/index.js
@@ -73,7 +73,6 @@ function closeModalByOverlayClick(evt) {
   // target is the element on which the event happened
   // currentTarget is the modal
   // if they are the same then we should close the modal
-  debugger;
   if (
     evt.target === evt.currentTarget ||
     evt.target.classList.contains("modal__close-button")
