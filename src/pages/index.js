1014c6463e92f45a91ce4f15415d07fb3d9a5e5f
Divya B <divyaaa1812@gmail.com>
2023-07-20 15:07:14 -0700

    changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,33 +7,6 @@ import PopupWithImage from "../components/PopupWithImage.js";
 import Api from "../components/Api.js";
 import "../pages/index.css";
 
-// const cardData = [
-//   {
-//     name: "Yosemite Valley",
-//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
-//   },
-//   {
-//     name: "Lake Louise",
-//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
-//   },
-//   {
-//     name: "Bald Mountains",
-//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
-//   },
-//   {
-//     name: "Latemar",
-//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
-//   },
-//   {
-//     name: "Vanoise National Park",
-//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
-//   },
-//   {
-//     name: "Lago di Braies",
-//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
-//   },
-// ];
-
 export const settings = {
   inputElementSelector: ".modal__text-input",
   submitButtonSelector: ".modal__button",
@@ -61,6 +34,7 @@ const profileSubtitleInputField = document.querySelector(
   "#profile-subtitle-input"
 );
 const avatarEditButton = document.querySelector(".profile__avatar-edit-button");
+
 const newCardPopup = new PopupWithForm(
   "#add-new-card",
   handleAddNewCardFormSubmit
