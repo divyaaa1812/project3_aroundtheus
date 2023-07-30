61ab72d3eef49f1bfb06eb21bc76144ef6e07e9a
Divya B <divyaaa1812@gmail.com>
2023-07-30 12:28:49 -0700

    changes

--- src/utils/constants.js
+++
@@ -1,8 +0,0 @@
-export const settings = {
-  inputElementSelector: ".modal__text-input",
-  submitButtonSelector: ".modal__button",
-  inactiveButtonSelector: "modal__button_disabled",
-  inputErrorSelector: "modal__input-error_visible",
-  errorSelectorHide: "modal__input-error-hide",
-  errorSelector: "modal__input-error",
-};
